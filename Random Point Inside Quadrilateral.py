a23bac7d062c836c91387344df165ffa5cb944b9
Atishay Jain <115044833+atti13@users.noreply.github.com>
2024-07-24 06:21:05 +0530

    Add files via upload

---
+++ Random Point Inside Quadrilateral.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import random
+from shapely.geometry import Polygon, Point
+
+def generate_random_point_in_quadrilateral(xTL, yTL, xTR, yTR, xBR, yBR, xBL, yBL):
+    # Define the quadrilateral using the provided corner coordinates
+    coords = [(xTL, yTL), (xTR, yTR), (xBR, yBR), (xBL, yBL)]
+    pgon = Polygon(coords)
+    
+    # Get the bounding box of the polygon
+    min_x, min_y, max_x, max_y = pgon.bounds
+    
+    while True:
+        random_point = Point(random.uniform(min_x, max_x), random.uniform(min_y, max_y))
+        if pgon.contains(random_point):
+            return random_point.x, random_point.y
+
+print("Enter the coordinates of the four corners of the quadrilateral:")
+xTL, yTL = map(float, input("Enter x and y for Top Left corner (separated by a space): ").split())
+xTR, yTR = map(float, input("Enter x and y for Top Right corner (separated by a space): ").split())
+xBR, yBR = map(float, input("Enter x and y for Bottom Right corner (separated by a space): ").split())
+xBL, yBL = map(float, input("Enter x and y for Bottom Left corner (separated by a space): ").split())
+
+xR, yR = generate_random_point_in_quadrilateral(xTL, yTL, xTR, yTR, xBR, yBR, xBL, yBL)
+print(f"Random point inside the quadrilateral: ({xR}, {yR})")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
